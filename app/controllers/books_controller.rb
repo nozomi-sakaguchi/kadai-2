e9d7d717fb49b11707331cb90a309eea4a044731
nozomi-sakaguchi <you@example.com>
2023-02-22 09:21:17 +0000

    4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book =  Book.new(book_params)
-    book.save
-    redirect_to'/books'
+    @book =  Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books =Book.all
+      render:index
+    end
+
   end
 
   def show
@@ -25,15 +31,16 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(book.id)
     else
-      render("books/edit")
+      render :edit
     end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
-
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
 
